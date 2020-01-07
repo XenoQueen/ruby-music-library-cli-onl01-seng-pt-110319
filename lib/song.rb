5e7b3ec8a8a1bc0084c7b6cc0a701fa474378fd9
Maika Kailani <queenmab051@gmail.com>
2020-01-07 14:54:00 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,55 @@
+require './concerns/findable.rb'
+class Song
+  
+  extend Concerns::Findable
+  attr_accessor :name, :genre, :artist
+  @@all = []
+  
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+  
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+  
+  def genre=(genre)
+    @genre = genre
+    if !genre.songs.include?(self)
+      genre.songs << self
+    end
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all = []
+  end
+  
+  def save
+    @@all << self 
+  end
+  
+  def self.create(name)
+    s = self.new(name)
+    s.save
+    s
+  end
+  
+  def self.new_from_filename(file)
+    name = file.gsub(".mp3", "").split (" - ")
+    song_name = name[1]
+    artist_name = Artist.find_or_create_by_name(name[0])
+    genre_name = Genre.find_or_create_by_name(name[2])
+    self.new(song_name, artist_name, genre_name)
+  end
+  
+  def self.create_from_filename(file)
+    @@all << self.new_from_filename(file)
+  end
+end

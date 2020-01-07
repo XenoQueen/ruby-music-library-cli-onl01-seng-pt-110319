dc63855a60e37455f91994533e14974dd2a83a03
Maika Kailani <queenmab051@gmail.com>
2020-01-07 18:11:42 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,32 @@
+#require './concerns/findable.rb'
+class Song
+  
+  #extend Concerns::Findable
+  attr_accessor :name
+  attr_reader :genre, :artist
+  @@all = []
+  
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    all.clear
+  end
+  
+  def save
+    self.class.all << self
+  end
+  
+  def self.create(name)
+    song = new(name)
+    song.save
+    song
+  end
+end

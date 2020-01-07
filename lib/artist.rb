49ce6c559a1b93d3faf4b5882ea536f25a1f0bcc
Maika Kailani <queenmab051@gmail.com>
2020-01-07 19:16:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -32,7 +32,7 @@ class Artist
   def add_song(song)
     # song.artist = self unless song.artist
     # songs << song unless songs.include?(song)
-
+      
       song.artist = self
       songs << song
     end

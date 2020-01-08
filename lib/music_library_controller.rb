df1e8b667530178443cc31190734462c4d1e31bf
Maika Kailani <queenmab051@gmail.com>
2020-01-08 08:24:02 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -52,7 +52,7 @@ class MusicLibraryController
     puts "Please enter the name of an artist:"
     input = gets.strip
     if artist = Artist.find_by_name(input)
-      artist.songs.sort{|a,b| a.name <=> b.name}.each_with_index{|song, index| puts "#{index} + 1}. #{song.name} - #{song.genre.name}"}
+      artist.songs.sort{|a,b| a.name <=> b.name}.each_with_index{|song, index| puts "#{index + 1}. #{song.name} - #{song.genre.name}"}
     end
   end
   
@@ -60,7 +60,7 @@ class MusicLibraryController
     puts "Please enter the name of a genre:"
     input = gets.strip
     if genre = Genre.find_by_name(input)
-      genre.songs.sort{|a,b| a.name <=> b.name}.each_with_index{|song, index| puts "#{index} + 1}. #{song.artist.name} - #{song.name}"}
+      genre.songs.sort{|a,b| a.name <=> b.name}.each_with_index{|song, index| puts "#{index + 1}. #{song.artist.name} - #{song.name}"}
     end
   end
   

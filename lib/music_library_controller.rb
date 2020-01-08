286ce124c534c8e6d1174828d5e8f8e9897d2187
Maika Kailani <queenmab051@gmail.com>
2020-01-08 07:07:34 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -22,7 +22,7 @@ class MusicLibraryController
   end
   
   def list_songs
-    Song.all.sort{|a,b| b.name<=>}.each_with_index{|song, index| puts "#{index} + 1}. #{song.artist} -  #{song.name} - #{song.genre}"}
+    Song.all.sort{|a,b| b.name<=>}.each_with_index{|song, index| puts "#{index} + 1}. #{song.artist.name} -  #{song.name} - #{song.genre.name}"}
   end
   
 end

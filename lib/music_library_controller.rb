29c0a44a0f43fd878c974127cfdc02de9ea397bd
Maika Kailani <queenmab051@gmail.com>
2020-01-08 08:11:30 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -70,5 +70,6 @@ class MusicLibraryController
     if (0..Song.all.length).include?(input.to_i)
       song = Song.all.sort{|a,b| a.name <=> b.name}[input.to_i - 1]
       puts "Playing #{song.name} by #{song.artist.name}."
+    end
   end
 end

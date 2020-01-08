ca258b0e9b26810ce62a99cd6db3a1172e4d2c7a
Maika Kailani <queenmab051@gmail.com>
2020-01-08 08:07:04 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -22,6 +22,11 @@ class MusicLibraryController
       case input
       when "list songs"
         list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+        
     end
   end
   

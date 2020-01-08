79c1d2854468b6183b18a623b3a2601d76905c2e
Maika Kailani <queenmab051@gmail.com>
2020-01-08 06:08:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,6 +51,7 @@ class Song
   end
   
   def self.new_by_filename(file)
+    
     artist, song, genre = file.split("-") # more than one variable assigns all to each element
     song_artist = Artist.find_or_create_by_name(artist)
     genre = genre.gsub(".mp3", "")

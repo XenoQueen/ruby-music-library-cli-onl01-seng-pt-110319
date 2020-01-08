95479de7e192e620f1057637052a4cba8eb9983e
Maika Kailani <queenmab051@gmail.com>
2020-01-08 06:03:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,6 +52,6 @@ class Song
   
   def self.new_by_filename(file)
     artist, song = file.split("-") # more than one variable assigns all to each element
-    song_artist = Artist.find_or_create_by_name 
+    song_artist = Artist.find_or_create_by_name(artist) 
   end
 end
